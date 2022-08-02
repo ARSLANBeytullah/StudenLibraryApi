1be6551fb5b4e649fb59c3ceaa2db4489c1839cf
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2022-08-02 15:06:00 +0300

    Student Controller GetAll,GetById,Count endpoints completed

--- StudentLibrary.Api/Controllers/StudentController.cs
+++ StudentLibrary.Api/Controllers/StudentController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using StudentLibrary.Core.Services;
+using System.Threading.Tasks;
 
 namespace StudentLibrary.Api.Controllers
 {
@@ -13,6 +14,26 @@ namespace StudentLibrary.Api.Controllers
         public StudentController(IStudentService studentService)
         {
             _studentService = studentService;
+        }
+
+
+        public async Task<IActionResult> GetAll()
+        {
+            var students = await _studentService.GetAllAsync();
+            return Ok(students);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var student = await _studentService.GetByIdAsync(id);
+            return Ok(student);
+        }
+
+        public async Task<IActionResult> Count()
+        {
+            int count = await _studentService.CountAsync();
+            return Ok(count);
+
         }
     }
 }
